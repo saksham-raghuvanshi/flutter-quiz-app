bd72ef1952fc1e99029b2cdba9abc93334245909
Saksham <sakshamraghuvanshi123@gmail.com>
2024-03-28 22:37:39 +0530

    Answer and Question Widget

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:quiz_app/answer_button.dart";
 
 class QuestionsScreen extends StatefulWidget {
   @override
@@ -10,6 +11,23 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text("Question Screen");
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The question...',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answerText: 'Answer Text1 ', onTap: () {}),
+          AnswerButton(answerText: 'Answer Text2 ', onTap: () {}),
+          AnswerButton(answerText: 'Answer Text3 ', onTap: () {}),
+        ],
+      ),
+    );
   }
 }
